101f2f059818262c92677a5012c88fcb9cd1e66a
sunha20 <c.seonha20@gmail.com>
2025-08-29 21:16:45 +0900

    Chore: 일단 돌아가게 주석처리

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
-from app.routers import image
+# from app.routers import image
 
 app = FastAPI()
 
-app.include_router(image.router, prefix="/images", tags=["images"])
+# app.include_router(image.router, prefix="/images", tags=["images"])
 
 @app.get("/health")
 def health_check():
