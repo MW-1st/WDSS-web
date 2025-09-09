b9a10e0ecf228c8a99a688759c4f124bad65f92b
volume5522 <volume5522>
2025-09-10 06:28:04 +0900

    feat : mvp2 allow_origins 적용

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,6 +17,8 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "http://3.85.51.152:5173",
+        "http://44.204.161.252:5173",
+        "http://44.204.161.252:8000",
     ],
     allow_credentials=True,
     allow_methods=["*"],
